ac7d2491c3e9425a46a1db1c3e9c3a8907f40606
Viktoria Adorjan <va520@ic.ac.uk>
2024-04-23 13:21:39 +0100

    test: added tests for Recipe and RecipeBook.

---
+++ test/recipe_test.dart
@@ -0,0 +1,81 @@
+import 'package:budget_meal/RecipeBook/RecipeBookModel.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('empty recipe book returns empty list for getRecipes', () {
+    RecipeBook recipeBook = RecipeBook();
+
+    assert (recipeBook.getRecipes().isEmpty);
+  });
+
+  test('empty recipe book returns empty list for getCategories', () {
+    RecipeBook recipeBook = RecipeBook();
+
+    assert (recipeBook.getCategories().isEmpty);
+  });
+
+  test('recipe book with one recipe returns singular list for getRecipes', () {
+    RecipeBook recipeBook = RecipeBook();
+    Recipe cerealRecipe = Recipe("Cereal", 1, [], false);
+
+    recipeBook.addRecipe(cerealRecipe);
+
+    assert (recipeBook.getRecipes().length == 1);
+    assert (recipeBook.getRecipeNames().length == 1);
+
+  });
+
+  test('recipe book with one recipe returns singular list for getCategories', () {
+    RecipeBook recipeBook = RecipeBook();
+    Recipe cerealRecipe = Recipe("Cereal", 1, [], false);
+
+    recipeBook.addRecipe(cerealRecipe);
+
+    assert (recipeBook.getCategories().length == 1);
+
+  });
+
+  test('recipe with undefined category returns Uncategorised category for getCategory', () {
+    Recipe cerealRecipe = Recipe("Cereal", 1, [], false);
+
+    assert (cerealRecipe.getCategory() == "Uncategorised");
+
+  });
+
+  test('recipe with breakfast category returns breakfast category for getCategory', () {
+    Recipe cerealRecipe = Recipe("Cereal", 1, [], false, "Breakfast");
+
+    assert (cerealRecipe.getCategory() == "Breakfast");
+
+  });
+
+  test('two recipes with the same category returns a singular list for getCategories', () {
+    RecipeBook recipeBook = RecipeBook();
+
+    Recipe cerealRecipe = Recipe("Cereal", 1, [], false);
+    Recipe sandwichRecipe = Recipe("Sandwich", 1, [], false);
+
+    recipeBook.addRecipe(cerealRecipe);
+    recipeBook.addRecipe(sandwichRecipe);
+
+    assert (recipeBook.getCategories().length == 1);
+    assert (recipeBook.getCategories().first == "Uncategorised");
+
+  });
+
+  test('two recipes with the different categories returns a list length of two for getCategories', () {
+    RecipeBook recipeBook = RecipeBook();
+
+    Recipe cerealRecipe = Recipe("Cereal", 1, [], false, "Breakfast");
+    Recipe sandwichRecipe = Recipe("Sandwich", 1, [], false);
+
+    recipeBook.addRecipe(cerealRecipe);
+    recipeBook.addRecipe(sandwichRecipe);
+
+    assert (recipeBook.getCategories().length == 2);
+    assert (recipeBook.getCategories().first == "Breakfast");
+    assert (recipeBook.getCategories().last == "Uncategorised");
+
+  });
+
+}
